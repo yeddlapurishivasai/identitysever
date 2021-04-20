b710e920417bd491251ad08bb941365b153e10ba
Shiva Sai Yeddlapuri <yeddlapurishivasai@hotmail.com>
2021-04-20 16:34:52 +0530

    ASP.NET web app acting as identity server.

---
+++ src/IdentityServer/IdentityServerWeb/Clients.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityServer4.Models;
+
+namespace IdentityServerWeb
+{
+    internal class Clients
+    {
+        public static IEnumerable<Client> Get()
+        {
+            return new List<Client>
+        {
+            ////new Client
+            ////{
+            ////    ClientId = "oauthClient",
+            ////    ClientName = "Example client application using client credentials",
+            ////    AllowedGrantTypes = GrantTypes.CodeAndClientCredentials,
+            ////    ClientSecrets = new List<Secret> {new Secret("SuperSecretPassword".Sha256())}, // change me!
+            ////    AllowedScopes = new List<string> {"api1.read"}
+            ////}
+            ///
+            new Client {
+                ClientId = "angular_spa",
+                ClientName = "Angular 4 Client",
+                AllowedGrantTypes = GrantTypes.Implicit,
+                AllowedScopes = new List<string> { "openid", "profile", "api1" },
+                RedirectUris = new List<string> { "http://localhost:4200/auth-callback" },
+                PostLogoutRedirectUris = new List<string> { "http://localhost:4200/" },
+                AllowedCorsOrigins = new List<string> { "http://localhost:4200" },
+                AllowAccessTokensViaBrowser = true
+            }
+        };
+        }
+    }
+}
---
+++ src/IdentityServer/IdentityServerWeb/Users.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using IdentityModel;
+using IdentityServer4.Test;
+
+namespace IdentityServerWeb
+{
+    internal class Users
+    {
+        public static List<TestUser> Get()
+        {
+            return new List<TestUser> {
+            new TestUser {
+                SubjectId = "5BE86359-073C-434B-AD2D-A3932222DABE",
+                Username = "ssy",
+                Password = "password",
+                Claims = new List<Claim> {
+                    new Claim(JwtClaimTypes.Email, "ssy@ssy90.com"),
+                    new Claim(JwtClaimTypes.Role, "admin")
+                }
+            }
+        };
+        }
+    }
+}
